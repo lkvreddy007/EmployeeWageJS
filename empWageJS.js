c9bd42d818059d04e96d1bbfa8b027d7402a36aa
lkvreddy007 <lkvreddy007@gmail.com>
2020-11-08 22:29:09 +0530

    [Krishna]added-abilityToCalculateEmployeeWagesForAMonth

--- empWageJS.js
+++ empWageJS.js
@@ -8,9 +8,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
-
-    
 function getWorkingHours(empCheck){
     switch (empCheck) {
         case IS_PART_TIME:
@@ -22,7 +21,10 @@ function getWorkingHours(empCheck){
     }
 }
 
-let empCheck = Math.floor(Math.random()*10) % 3; 
-let empHrs = getWorkingHours(empCheck);   
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Hours: "+empHrs+" Emp Wage: "+empWage);
+let totalEmpHrs = 0;
+for (let day =0; day < NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random()*10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}   
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Hours: " + totalEmpHrs + " Emp Wage: " + empWage);
